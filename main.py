a9a616665e6f93e16cd04071b580b4e05d6898f3
Tyler694 <141347012+Tyler694@users.noreply.github.com>
2026-02-13 20:48:44 +0000

    Temporary Raycaster

--- main.py
+++ main.py
@@ -20,21 +20,142 @@ def drawMap():
                 pygame.draw.rect(screen, (50, 50, 50), (j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE-1, TILE_SIZE-1))
             else:
                 pygame.draw.rect(screen, (0,0,0), (j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE-1, TILE_SIZE-1))
+
 def get_tile(px, py):
     return px // 100, py // 100
 
-def findHorizontalLine(i):
-    if player.angle > (3 * math.pi) / 2 or player.angle < math.pi / 2:
-        pygame.draw.circle(screen, (255,0,0), (tileX * 100 + TILE_SIZE * (i + 1), player.y + ((math.tan((player.angle)) * ((tileX * 100 + TILE_SIZE * (i + 1)) - player.x)))), 5)
-    else:
-        pygame.draw.circle(screen, (255, 0, 0), (tileX * 100 - TILE_SIZE * i, player.y + ((math.tan((player.angle))) * ((tileX * 100 - TILE_SIZE * i) - player.x))), 5)
-def findVerticalLine(i):
-    print(player.angle * 180 / math.pi)
-    pygame.draw.circle(screen, (255,0,0), (player.x - ((math.tan((player.angle - ((3 * math.pi) / 2))) * ((tileY * 100 - TILE_SIZE * i) - player.y))), tileY * 100 - TILE_SIZE * i), 5)
+def findHorizontalLine():
+
+        if player.angle > (3 * math.pi) / 2 or player.angle < math.pi / 2:
+            #East
+            i = 0
+            x = tileX * 100 + TILE_SIZE * i
+            y = player.y + (math.tan((player.angle)) * ((tileX * 100 + TILE_SIZE * i) - player.x))
+            if x >= 800:
+                x = 700
+            if y <= 0:
+                y = 100
+            if y >= 800:
+                y = 700
+
+            while map[int(x // 100)][int(y // 100)] == 0:
+                x = tileX * 100 + TILE_SIZE * (i+1)
+                y = player.y + (math.tan((player.angle)) * ((tileX * 100 + TILE_SIZE * (i+1)) - player.x))
+
+                if x >= 800:
+                    break
+                if x <= 0:
+                    break
+                if y <= 0:
+                    break
+                if y >= 800:
+                    break
+
+
+                pygame.draw.circle(screen, (255,0,0), (x, y), 5)
+                i+=1
+
+
+        else:
+            #West
+            i = 0
+
+            x = tileX * 100 - TILE_SIZE * i
+            y = player.y + (math.tan((player.angle))) * ((tileX * 100 - TILE_SIZE * i) - player.x)
+
+            if x <= 0:
+                x = 100
+            if y >= 800:
+                y = 700
+            if y <= 0:
+                y = 100
+
+            while map[int(x // 100)][int(y // 100)] == 0:
+
+                x = tileX * 100 - TILE_SIZE * i
+                y = player.y + (math.tan((player.angle))) * ((tileX * 100 - TILE_SIZE * i) - player.x)
+
+                if x <= 0:
+                    break
+                if y >= 800:
+                    break
+                if y <= 0:
+                    break
+                pygame.draw.circle(screen, (255,0,0), (x, y), 5)
+
+                i+=1
+        return x,y
+def findVerticalLine():
+        #North
+        if player.angle < 0 or player.angle > math.pi:
+            i = 0
+            x = player.x - (math.tan((player.angle - ((3 * math.pi) / 2))) * ((tileY * 100 - TILE_SIZE * i) - player.y))
+            y = tileY * 100 - TILE_SIZE * i
+            if x > 800:
+                x = 700
+            if x < 0:
+                x = 100
+            if y <= 0:
+                y = 100
+
+            while map[int(x // 100)][int(y // 100)] == 0:
+                x = player.x - (math.tan((player.angle - ((3 * math.pi) / 2))) * ((tileY * 100 - TILE_SIZE * i) - player.y))
+                y = tileY * 100 - (TILE_SIZE * i)
+
+                if x > 800:
+                    break
+                if x < 0:
+                    break
+                if y <= 0:
+                    break
+                pygame.draw.circle(screen, (255,0,0), (x, y), 5)
+
+                i += 1
+        else:
+                i = 0
+                #South
+                x = player.x - (math.tan((player.angle - ((3 * math.pi) / 2))) * ((tileY * 100 + TILE_SIZE * i) - player.y))
+                y = tileY * 100 + TILE_SIZE * i
+
+                if x >= 800:
+                    x = 700
+                if x <= 0:
+                    x = 100
+                if y <= 0:
+                    y = 100
+
+                while map[int(x//100)][int(y//100)+1] == 0:
+                    x = player.x - (math.tan((player.angle - ((3 * math.pi) / 2))) * ((tileY * 100 + TILE_SIZE * (i + 1)) - player.y))
+                    y = tileY * 100 + TILE_SIZE * (i + 1)
+                    if x >= 800:
+                        break
+                    if x <= 0:
+                        break
+                    if y <= 0:
+                        break
+
+                    pygame.draw.circle(screen, (255, 0, 0), (x, y), 5)
+                    i+=1
+        return x,y
 def rayCast():
-        for i in range(5):
-            findHorizontalLine(i)
-            findVerticalLine(i)
+    HITXH,HITYH = findHorizontalLine()
+    HITXV,HITYV = findVerticalLine()
+
+
+    HorizontalDist = math.sqrt((abs(HITXH - player.x)*abs(HITXH - player.x)) + (abs(HITYH - player.y)*abs(HITXH - player.x)))
+    VerticalDist = math.sqrt((abs(HITXV - player.x)*abs(HITXV - player.x)) + (abs(HITYV - player.y)*abs(HITXV - player.x)))
+
+    #print("Vertical",VerticalDist, "Horizontal", HorizontalDist)
+
+    if VerticalDist > HorizontalDist:
+        #Horizontal LEFT RIGHT
+        pygame.draw.line(screen, (255,0,0), (player.x, player.y), (HITXH, HITYH))
+    else:
+        #Vertical TOP BOTTOM
+        pygame.draw.line(screen, (0,255,0), (player.x, player.y), (HITXV, HITYV))
+
+
+
 
 while running:
     screen.fill((70,70,70))
--- player.py
+++ player.py
@@ -6,12 +6,11 @@ class Player:
     def __init__(self):
         self.x = 200
         self.y = 200
-        self.angle = 0
+        self.angle = 6.28
         self.screen = pygame.display.get_surface()
 
     def draw(self):
         pygame.draw.circle(self.screen, (255,0,0), (self.x, self.y), 5)
-        pygame.draw.line(self.screen, (255,0,0), (self.x, self.y), (self.x + math.cos(self.angle) * 50, self.y + math.sin(self.angle) * 50))
     def move(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
